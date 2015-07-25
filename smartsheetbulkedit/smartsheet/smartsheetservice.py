1d8aaf91e904b45f35fe07d05dca4c964d81cb7b
Scott Wimer <scott.wimer@smartsheet.com>
2015-07-25 15:52:30 -0700

    Externalize the looping over Sheets
    Added methods that take a list of SheetInfo object, rather than working
    with an internally derived list of Sheets.  These methods also return
    two lists, the SheetInfo objects for the Sheets that were updated, and
    the SheetInfo objects (along with a bit of additional info), for those
    Sheets that were not updated.
    
    This will enable retrying of bulk operations in the event of
    Sheet-specific failures.

--- smartsheetbulkedit/smartsheet/smartsheetservice.py
+++ smartsheetbulkedit/smartsheet/smartsheetservice.py
@@ -1,8 +1,10 @@
 import logging
+import sys
 
 from smartsheetclient import Column
 from smartsheetclient import RowPositionProperties
 from smartsheetclient import SmartsheetClient
+from smartsheetclient import SheetInfo
 
 from smartsheetbulkedit import SmartsheetBulkEditError
 
@@ -29,6 +31,33 @@ class SmartsheetService(object):
 			sheet = self.__getSheetIfInWorkspace(sheetInfo, workspace)
 			if sheet is not None:
 				self.updateCell(sheet, rowNumber, columnIndex=columnIndex, columnTitle=columnTitle, value=value)
+
+	def updateCellInSheetList(self, rowNumber, columnIndex=None, columnTitle=None, value=None, sheetInfoList=None):
+		'''
+		Update the specified Cell in the list of sheets.
+		The Cell is identified by rowNumber and columnIndex or columnTitle.
+
+		Two lists are returned, the first is a list of the SheetInfo objects
+		for the Sheets that were successfully updated.
+		The second returned list is a list of 3-tuples:
+		    (SheetInfo, Exception, stacktrace) for the sheets that were NOT
+		updated successfully.
+		'''
+		good, bad = [], []
+		if columnIndex is not None and columnTitle is not None:
+			raise SmartsheetBulkEditError('one but not both "columnIndex" and "columnTitle" must be specified')
+		elif columnTitle is not None:
+			columnIndex = sheet.getColumnsInfo().getColumnByTitle(columnTitle).index
+		elif columnIndex is None:
+			raise SmartsheetBulkEditError('either "columnIndex" or "columnTitle" must be specified')
+		for sheetInfo in sheetInfoList:
+			try:
+				sheet = sheetInfo.loadSheet()
+				self.updateCell(sheet, rowNumber=rowNumber, columnIndex=columnIndex, columnTitle=columnTitle, value=value)
+				good.append(sheet)
+			except Exception as e:
+				bad.append((sheetInfo, e, sys.exc_info()[2]))
+		return good, bad
 
 	def addColumn(self, sheet, title, index=None, type=None, options=None, symbol=None, isPrimary=None, systemColumnType=None, autoNumberFormat=None, width=None):
 		params = {}
@@ -66,6 +95,28 @@ class SmartsheetService(object):
 					systemColumnType=systemColumnType, 
 					autoNumberFormat=autoNumberFormat, 
 					width=width)
+
+	def addColumnInSheetList(self, title, workspace=None, index=None, type=None, options=None, symbol=None, isPrimary=None, systemColumnType=None, autoNumberFormat=None, width=None, sheetInfoList=None):
+		good, bad = [], []
+		for sheetInfo in sheetInfoList:
+			try:
+				sheet = sheetInfo.loadSheet()
+				self.addColumn(
+					sheet,
+					title,
+					index=index,
+					type=type,
+					options=options,
+					symbol=symbol,
+					isPrimary=isPrimary,
+					systemColumnType=systemColumnType,
+					autoNumberFormat=autoNumberFormat,
+					width=width)
+				good.append(sheetInfo)
+			except Exception as e:
+				bad.append((sheetInfo, e, sys.exc_info()[2]))
+		return good, bad
+
 
 	def updateColumn(self, sheet, oldTitle, newTitle=None, index=None, type=None, options=None, symbol=None, systemColumnType=None, autoNumberFormat=None, width=None, format=None):
 		column = sheet.getColumnsInfo().getColumnByTitle(oldTitle)
@@ -106,6 +157,28 @@ class SmartsheetService(object):
 					width=width, 
 					format=format)
 
+	def updateColumnInSheetList(self, oldTitle, workspace=None, newTitle=None, index=None, type=None, options=None, symbol=None, systemColumnType=None, autoNumberFormat=None, width=None, format=None, sheetInfoList=None):
+		good, bad = [], []
+		for sheetInfo in sheetInfoList:
+			try:
+				sheet = sheetInfo.loadSheet()
+				self.updateColumn(
+					sheet,
+					oldTitle,
+					newTitle=newTitle,
+					index=index,
+					type=type,
+					options=options,
+					symbol=symbol,
+					systemColumnType=systemColumnType,
+					autoNumberFormat=autoNumberFormat,
+					width=width,
+					format=format)
+				good.append(sheetInfo)
+			except Exception as e:
+				bad.append((sheetInfo, e, sys.exc_info()[2]))
+		return good, bad
+
 	def addRow(self, sheet, rowDictionary, rowNumber=None):
 		row = sheet.makeRow(**rowDictionary)
 		if rowNumber is None:
@@ -127,6 +200,17 @@ class SmartsheetService(object):
 			if sheet is not None:
 				self.addRow(sheet, rowDictionary, rowNumber)
 
+	def addRowInSheetList(self, rowDictionary, rowNumber=None, sheetInfoList=None):
+		good, bad = [], []
+		for sheetInfo in sheetInfoList:
+			try:
+				sheet = sheetInfo.loadSheet()
+				self.addRow(sheet, rowDictionary, rowNumber=rowNumber)
+				good.append(sheetInfo)
+			except Exception as e:
+				bad.append((sheetInfo, e, sys.exc_info()[2]))
+		return good, bad
+		
 	def expandAllRows(self, sheet, isExpanded=True):
 		# operate only on rows referenced to be parent rows
 		parentRowNumbers = frozenset([row.parentRowNumber for row in sheet.rows if row.parentRowNumber])
@@ -142,9 +226,56 @@ class SmartsheetService(object):
 			if sheet is not None:
 				self.expandAllRows(sheet, isExpanded)
 
+	def expandAllRowsInSheetList(self, isExpanded=True, sheetInfoList=None):
+		good, bad = [], []
+		for sheetInfo in sheetInfoList:
+			try:
+				sheet = sheetInfo.loadSheet()
+				self.expandAllRows(sheet, isExpanded=isExpanded)
+				good.append(sheetInfo)
+			except Exception as e:
+				bad.append((sheetInfo, e, sys.exc_info()[2]))
+		return good, bad
+
 	def getSheetInfos(self, workspace=None):
 		# Smartsheet Python SDK cannot filter by workspace
 		return self.__smartsheetClient.fetchSheetList()
+
+	def getSheetInfosInWorkspace(self, workspaceID=''):
+		'''
+		Get a list of the SheetInfo objects in the specified workspace.
+		'''
+		# Uses the Smartsheet Python SDK client directly, since this
+		# functionality isn't yet implemented in the high-level Python SDK.
+		sheetInfoList = []
+		try:
+			workspacePath = '/workspace/%s' % workspaceID
+			workspace = self.__smartsheetClient.GET(workspacePath)
+			for sheet_fields in workspace['sheets']:
+				sheetInfoList.append(SheetInfo(sheet_fields, self.__smartsheetClient))
+			return sheetInfoList
+		except Exception as e:
+			self.__logger.exception("Error getting list of sheets from workspace ID: %s: %r", workspaceID, e)
+			raise
+			raise (SmartsheetBulkEditError, ("Error getting list of sheets from workspace ID: %s: %r" % (workspaceID, e)), sys.exc_info()[2])
+
+	def getWorkspacesByName(self, workspaceName):
+		'''
+		Get a list of workspaces that have the given name.
+		The returned workspaces are Python dicts, not objects from the
+		Smartsheet client library -- it does not yet support workspaces.
+		'''
+		# Uses the Smartsheet Python SDK client directly, since this
+		# functionality isn't yet implemented by the high-level Python SDK.
+		workspaces = []
+		try:
+			for workspace in self.__smartsheetClient.GET('/workspaces'):
+				if workspace['name'] == workspaceName:
+					workspaces.append(workspace)
+			return workspaces
+		except Exception as e:
+			raise (SmartsheetBulkEditError, ("Error getting workspace list: %r" % e), sys.exc_info()[2])
+		raise SmartsheetBulkEditError("Unable to find workspace named: '%s'" % workspaceName)
 
 	def __getSheetIfInWorkspace(self, sheetInfo, workspace):
 		""" Returns a Sheet if it belongs to the specified workspace
